78182e58bec1c711225d57e001bc2d2d6b734b85
pinklaosa <rps53009@hotmail.com>
2022-05-29 18:32:40 +0700

    Hero

--- pages/homepage/index.js
+++ pages/homepage/index.js
@@ -1,16 +1,33 @@
 import Head from "next/head";
 
 const HomePage = () => {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
 
-    return(
-        <div className="">
-            <Head>
-                <title>Home</title>
-            </Head>
-            <h2>
-                HomePage
-            </h2>
+      <div className="">
+        <div className="bg-gray-800 p-2">
+          <div className="flex flex-row">
+            <div className="flex p-36 basis-3/6">
+              <div className="flex flex-col font-serif text-white">
+                <div className="flex">
+                  <h1 className="text-7xl ">Welcome to Motor</h1>
+                </div>
+                <div className="border-2 my-6"></div>
+                <div className="flex">
+                    <p> </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex p-5 basis-3/6 justify-center">
+              <img src="../assets/homepage/H01.png" className="w-1/1" />
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 export default HomePage;
